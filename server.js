ac22edc73e4a9578401c4be126cba4643b1662e1
Daniel Castilho <dan.castilho@gmail.com>
2021-03-19 12:20:30 -0400

    Create ShortUrl Schema

--- server.js
+++ server.js
@@ -1,10 +1,18 @@
 const express = require('express');
+const mongoose = require('mongoose');
 const app = express();
+
+mongoose.connect('mongodb+srv://tyny:j60w9P2auUXwVcVK@cluster0.sx04z.mongodb.net/tyny?retryWrites=true&w=majority', {
+    useNewUrlParser: true, useUnifiedTopology: true });
 
 app.set('view engine', 'ejs');
 
 app.get('/', (req, res) => {
     res.render('index');
+})
+
+app.post('/shortUrls', (req, res) => {
+
 })
 
 app.listen(process.env.PORT || 3000);

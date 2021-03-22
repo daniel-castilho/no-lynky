1b0ff618858fe5caf5fe03985595f5c49243d098
Daniel Castilho <dan.castilho@gmail.com>
2021-03-22 13:14:10 -0400

    Heroku

--- server/server.js
+++ server/server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const database = require('./infra/db');
 const app = express();
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 // Connect to Database
 database();
